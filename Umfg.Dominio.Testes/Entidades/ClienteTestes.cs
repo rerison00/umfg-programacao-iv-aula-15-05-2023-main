d78e8a8d5e5f822b120a0606f83cb09b3cf8f00b
Rerison <rerison-olivo@hotmail.com>
2023-05-26 15:59:27 -0300

    Projeto

---
+++ Umfg.Dominio.Testes/Entidades/ClienteTestes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umfg.Dominio.Entidades;
+
+namespace Umfg.Dominio.Testes.Entidades
+{
+    [TestClass]
+    public sealed class ClienteTestes
+    {
+        private const string _firstOwner = "Juliano Maciel";
+        private const string _secondOwner = "Ruan";
+        private const string _category = "Cliente";
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_Construtor_Sucesso()
+        {
+            try
+            {
+                var cliente = new Cliente("04405125228", "TESTE");
+                
+                Assert.AreEqual("TESTE", cliente.Nome);
+                Assert.AreEqual("04405125228", cliente.Documento);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_DocumentoNull_ThrowArgumentNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(()
+                => new Cliente("321321321", "TESTE"));
+        }
+
+        [TestMethod]
+        [Owner(_firstOwner)]
+        [TestCategory(_category)]
+        public void Cliente_NomeNull_ThrowArgumentNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(()
+                => new Cliente("04405125228", "Teste"));
+        }
+    }
+}
---
+++ Umfg.Dominio/Entidades/Cliente.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umfg.Dominio.Entidades
+{
+    public sealed class Cliente
+    {
+        public string Documento { get; private set; }
+        public string Nome { get; private set; }
+
+        public Cliente(string documento, string nome)
+        {
+            if (string.IsNullOrEmpty(documento))
+            {
+                throw new ArgumentNullException(nameof(documento), "O documento não pode ser vazio.");
+            }
+
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentNullException(nameof(nome), "O nome não pode ser vazio.");
+            }
+
+            Documento = documento;
+            Nome = nome;
+        }
+    }
+
+
+}
